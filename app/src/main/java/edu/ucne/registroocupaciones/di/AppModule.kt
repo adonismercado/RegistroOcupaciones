2a86d9ad66dd08bc24fa10b0eac37ac09ab99732
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-05-07 17:03:19 -0400

    Crea instancia de base de datos en AppModule.kt

---
+++ app/src/main/java/edu/ucne/registroocupaciones/di/AppModule.kt
@@ -0,0 +1,26 @@
+package edu.ucne.registroocupaciones.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.registroocupaciones.data.ocupaciones.database.OcupacionDb
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideOcupacionDb(@ApplicationContext appContext: Context) =
+        Room.databaseBuilder(
+            appContext,
+            OcupacionDb::class.java,
+            "Ocupacion.db"
+        ).fallbackToDestructiveMigration()
+            .build()
+
+}
