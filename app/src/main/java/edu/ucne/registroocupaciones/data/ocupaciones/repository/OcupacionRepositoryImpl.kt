23f6cadbae878144b24446e3d76df090eff43398
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-05-07 17:27:36 -0400

    Crea repositorio o servicio OcupacionRepositoryImpl

---
+++ app/src/main/java/edu/ucne/registroocupaciones/data/ocupaciones/repository/OcupacionRepositoryImpl.kt
@@ -0,0 +1,28 @@
+package edu.ucne.registroocupaciones.data.ocupaciones.repository
+
+import edu.ucne.registroocupaciones.data.ocupaciones.local.OcupacionDao
+import edu.ucne.registroocupaciones.data.ocupaciones.mapper.toDomain
+import edu.ucne.registroocupaciones.data.ocupaciones.mapper.toEntity
+import edu.ucne.registroocupaciones.domain.ocupacion.model.Ocupacion
+import edu.ucne.registroocupaciones.domain.ocupacion.repository.OcupacionRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class OcupacionRepositoryImpl(
+    private val dao: OcupacionDao
+) : OcupacionRepository {
+    override fun observeOcupaciones(): Flow<List<Ocupacion>> = dao.observeAll().map { list ->
+        list.map { it.toDomain() }
+    }
+
+    override suspend fun getOcupacion(id: Int): Ocupacion? = dao.getById(id)?.toDomain()
+
+    override suspend fun upsert(ocupacion: Ocupacion): Int {
+        dao.upsert(ocupacion.toEntity())
+        return ocupacion.ocupacionId
+    }
+
+    override suspend fun delete(id: Int){
+        dao.deleteById(id)
+    }
+}
