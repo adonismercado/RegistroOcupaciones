beec26023e2485361a074395a779a1dbf8071be0
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-05-09 16:46:43 -0400

    cambia tipo de dato de sueldo de Double a String en EditOcupacionUiEvent

--- app/src/main/java/edu/ucne/registroocupaciones/presentation/edit/EditOcupacionUiEvent.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/presentation/edit/EditOcupacionUiEvent.kt
@@ -2,8 +2,8 @@ package edu.ucne.registroocupaciones.presentation.edit
 
 sealed interface EditOcupacionUiEvent {
     data class Load(val id: Int?) : EditOcupacionUiEvent
-    data class DescriptionChanged(val value: String) : EditOcupacionUiEvent
-    data class SueldoChanged(val value: Double) : EditOcupacionUiEvent
+    data class DescripcionChanged(val value: String) : EditOcupacionUiEvent
+    data class SueldoChanged(val value: String) : EditOcupacionUiEvent
     data object Save : EditOcupacionUiEvent
     data object Delete : EditOcupacionUiEvent
 }
