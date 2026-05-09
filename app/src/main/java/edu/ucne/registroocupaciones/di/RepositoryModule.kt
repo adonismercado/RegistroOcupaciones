ebf366f6537ae2a3553dc4d0659e7692c2b088e8
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-05-09 19:37:10 -0400

    crea clase RepositoryModule

---
+++ app/src/main/java/edu/ucne/registroocupaciones/di/RepositoryModule.kt
@@ -0,0 +1,19 @@
+package edu.ucne.registroocupaciones.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.registroocupaciones.data.ocupaciones.repository.OcupacionRepositoryImpl
+import edu.ucne.registroocupaciones.domain.ocupacion.repository.OcupacionRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract  class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindOcupacionRepository(
+        impl: OcupacionRepositoryImpl
+    ): OcupacionRepository
+}
