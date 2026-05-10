e049bc30c2724d7e65e22b1b34f5b6f28814801b
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-05-09 20:23:24 -0400

    agrega navegacion en EditOcupacionScreen

--- app/src/main/java/edu/ucne/registroocupaciones/presentation/edit/EditOcupacionScreen.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/presentation/edit/EditOcupacionScreen.kt
@@ -1,8 +1,10 @@
 package edu.ucne.registroocupaciones.presentation.edit
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
@@ -19,6 +21,8 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
@@ -33,6 +37,12 @@ fun EditOcupacionScreen(
     onBack: () -> Unit
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
+
+    if (state.saved || state.deleted) {
+        SideEffect {
+            onBack()
+        }
+    }
 
     Scaffold(
         topBar = {
@@ -72,6 +82,8 @@ fun EditOcupacionScreen(
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                 singleLine = true
             )
+
+            Spacer(modifier = Modifier.height(8.dp))
 
             Button(
                 onClick = { viewModel.onEvent(EditOcupacionUiEvent.Save) },
