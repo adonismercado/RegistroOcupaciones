d2f94affe7472bdd785529f7788aa7022f8f45a1
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-05-15 15:22:55 -0400

    crea clase ListEmpleadoUiState

---
+++ app/src/main/java/edu/ucne/registroocupaciones/presentation/empleado/list/ListEmpleadoUiState.kt
@@ -0,0 +1,12 @@
+package edu.ucne.registroocupaciones.presentation.empleado.list
+
+import edu.ucne.registroocupaciones.domain.ocupacion.model.Empleado
+
+data class ListEmpleadoUiState (
+    val isLoading: Boolean = false,
+    val empleados: List<Empleado> = emptyList(),
+    val message: String? = null,
+    val navigateToCreate: Boolean = false,
+    val navigateToEditId: Int? = null,
+    val error: String? = null
+)
